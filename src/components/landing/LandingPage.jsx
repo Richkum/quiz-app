1ec1c7e6749cbff7dac6785f648447e8b76fcd92
Richkum <kumatum2@gmail.com>
2023-11-28 12:55:04 +0100

    building the UI

--- src/components/landing/LandingPage.jsx
+++ src/components/landing/LandingPage.jsx
@@ -4,11 +4,16 @@ import { Link } from "react-router-dom";
 function LandingPage() {
   return (
     <>
-      <div>
-        <h1>Welcome to the Trivia App!</h1>
-        <p>Instructions: ...</p>
+      <div className="land-div">
+        <h1 className="h1">Welcome to the Trivia App!</h1>
+        <div className="instruct">
+          <h3 className="h3">Instructions: ...</h3>
+          <p className="p">the dkhu snduicvnecosdnvrekdm jsik</p>
+          <p className="p"> ujcndhsytfevcscodimcscv undcbvssvatvvfk</p>
+          <p className="p">okay tou cbkscf dnfkvcv msm</p>
+        </div>
         <Link to={"/question"}>
-          <button>Start Game</button>
+          <button className="btn">Start Game</button>
         </Link>
       </div>
     </>
--- src/components/question/QuestionPage.jsx
+++ src/components/question/QuestionPage.jsx
@@ -1,14 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useQuery } from "react-query";
 
 function QuestionPage() {
+  const urlApi =
+    "https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean";
+
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["quizData", urlApi],
+    queryFn: async () => {
+      const response = await fetch(urlApi);
+      const data = await response.json();
+      return data;
+    },
+  });
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+  console.log(data);
+
   return (
     <>
-      <div>Question Page</div>
-      <Link to={"/results"}>
-        {" "}
-        <button className="submit">Submit</button>
-      </Link>
+      <div className="quest-page">
+        <h1 className="h1">Please Answer All Quesions</h1>
+        <div className="questions"></div>
+
+        <Link to={"/results"}>
+          {" "}
+          <button className="submit">Submit</button>
+        </Link>
+      </div>
     </>
   );
 }
